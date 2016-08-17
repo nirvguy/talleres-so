0fd1e6311c7d269d9ac1b5f3a176ec745a859d7c
nirvguy <nirvguy@gmail.com>
2016-08-16 23:15:13 -0300

    Sube material del taller de syscalls

---
+++ taller-syscalls/src/launch.c
@@ -0,0 +1,35 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+	int status;
+	pid_t child;
+
+	if (argc <= 1) {
+		fprintf(stderr, "Uso: %s commando [argumentos ...]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Fork en dos procesos */
+	child = fork();
+	if (child == -1) { perror("ERROR fork"); return 1; }
+	if (child == 0) {
+		/* Solo se ejecuta en el hijo */
+		execvp(argv[1], argv+1);
+		/* Si vuelve de exec() hubo un error */
+		perror("ERROR child exec(...)"); exit(1);
+	} else {
+		/* Solo se ejecuta en el padre */
+		while(1) {
+			if (wait(&status) < 0) { perror("waitpid"); break; }
+			if (WIFEXITED(status)) break; /* Proceso terminado */
+		}
+	}
+	return 0;
+}
